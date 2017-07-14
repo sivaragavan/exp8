1a0bdd1537f6838ba1b516d6da8019ae21e50e33
Sivaprakash Ragavan <mail@sivragav.com>
2017-07-14 18:28:03 -0400

    Added a basic CLI utility to find al commands in a given file

---
+++ index.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+fs.readFile('sample/input.txt', 'utf8', function (err, contents) {
+
+  result = executeCommands(contents);
+
+  fs.writeFile("sample/output.html", result, function (err) {
+    if (err) {
+      return console.log("Error writing file");
+    }
+    console.log("The file was saved!");
+  });
+});
+
+function executeCommands(text) {
+  const regex = /\/CLO[^\n\/]*[\n\/]/gi;
+  let m;
+
+  while ((m = regex.exec(text)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+      console.log(`Found match at ${regex.lastIndex}, group ${groupIndex}: ${match}`);
+    });
+  }
+
+  return text;
+}
